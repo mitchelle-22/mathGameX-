2fce6a56162157d5616bbdee418e059fbde9f47f
damac53 <mitchellefetty@gmail.com>
2023-05-19 12:55:16 +0200

    Game Activity added

--- app/src/main/java/com/technical/mathgamepart1/MainActivity.java
+++ app/src/main/java/com/technical/mathgamepart1/MainActivity.java
@@ -3,8 +3,11 @@ package com.technical.mathgamepart1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
