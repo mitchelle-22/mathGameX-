ec15d0122dbdf14150dbeab2a784128d5fa00837
damac53 <mitchellefetty@gmail.com>
2023-05-22 10:41:32 +0200

    variables are created

--- app/src/main/java/com/technical/mathgamepart1/MainActivity.java
+++ app/src/main/java/com/technical/mathgamepart1/MainActivity.java
@@ -7,11 +7,20 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button addition;
+    Button subtraction;
+    Button multiplication;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         addition = findViewById(R.id.buttonAdd);
+         subtraction = findViewById(R.id.buttonSub);
+         multiplication = findViewById(R.id.buttonMulti);
+
+
     }
 }
