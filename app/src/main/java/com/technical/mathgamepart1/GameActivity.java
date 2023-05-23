61d4bbf9f3c6920201ca3f375e7f58aab8c3a734
damac53 <mitchellefetty@gmail.com>
2023-05-23 10:19:43 +0200

    game activity

--- app/src/main/java/com/technical/mathgamepart1/GameActivity.java
+++ app/src/main/java/com/technical/mathgamepart1/GameActivity.java
@@ -1,8 +1,11 @@
 package com.technical.mathgamepart1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                finish();
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        return true;
+
 }
