b410dd8fc2b3489d806f8bb45005b2e3d2ed9060
damac53 <mitchellefetty@gmail.com>
2023-05-23 13:42:47 +0200

    modief game activity

--- app/src/main/java/com/technical/mathgamepart1/MainActivity.java
+++ app/src/main/java/com/technical/mathgamepart1/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         addition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+               Intent intent = new Intent(MainActivity.this, GameActivity.class);
                startActivity(intent);
             }
         });
