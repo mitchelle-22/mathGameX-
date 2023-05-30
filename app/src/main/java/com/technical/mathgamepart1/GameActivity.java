688ab76376f9b39bbbfb16544702a0d5985dca46
damac53 <mitchellefetty@gmail.com>
2023-05-30 12:24:09 +0200

    game activity

--- app/src/main/java/com/technical/mathgamepart1/GameActivity.java
+++ app/src/main/java/com/technical/mathgamepart1/GameActivity.java
@@ -83,6 +83,8 @@ public class GameActivity extends AppCompatActivity {
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                gameContinue();
+                editTextAnswer.setText("");
 
             }
         });
